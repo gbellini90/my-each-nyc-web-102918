261574ed8c127fa52d9d64592826b06933adb86b
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-09 22:46:14 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,18 @@
-def my_each (array)
+def my_each(array)
+  if block_given?
   i=0 
+  
   while i<array.length 
-  yield (array[i])
+  yield(array[i])
   i+=1 
-  end
+end
   array
+else
+  puts "No block given"
+  end 
 end
 
 my_each(array) do |i|
   puts i 
 end
 
-my_each(collection)
