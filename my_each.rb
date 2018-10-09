1e433cffbb11d7c0ecd65ba645bbaecfadbae210
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-09 23:04:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,19 +1,19 @@
 def my_each(array)
-  if block_given?
-  i=0 
+   if block_given?
+   i=0 
   
-  while i<array.length 
-  yield(array[i])
-  i+=1 
-end
-  array
-else
-  "No block given"
+   while i<array.length 
+   yield(array[i])
+   i+=1 
+  end
+   array
+   else
+    "No block given"
   end 
 end
 
-my_each(array) do |i|
+my_each(collection) do |i|
    i 
 end
 
-my_each(words)
+
